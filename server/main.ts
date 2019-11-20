4ed71d77206c5de1004e479e83cef377a0bb7dd9
Veronika Baraniak <veronika.baraniak@eleks.com>
2019-11-20 17:58:36 +0200

    compress bundle

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { enableProdMode } from '@angular/core';
 import { ApplicationModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+  enableProdMode();
+  app.use(compression());
   await app.listen(process.env.PORT || 4200);
 }
 bootstrap();
